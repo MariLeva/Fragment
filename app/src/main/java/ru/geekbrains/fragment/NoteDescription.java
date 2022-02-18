f1862e3f441f60108505466d6a060c4c9c43d647
mari_leva <savosh999@yandex.ru>
2022-02-19 00:40:36 +0700

    Урок 8. Добавила меню во фрагмент описание заметки

--- app/src/main/java/ru/geekbrains/fragment/NoteDescription.java
+++ app/src/main/java/ru/geekbrains/fragment/NoteDescription.java
@@ -7,6 +7,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -29,6 +32,7 @@ public class NoteDescription extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_note_description, container, false);
     }
 
@@ -43,6 +47,15 @@ public class NoteDescription extends Fragment {
             tvName.setText(noteMain.getNoteName());
             tvDate.setText(format.format(noteMain.getDate()));
             getChildFragmentManager().beginTransaction().replace(R.id.description_fragment, NoteDescriptionChild.newInstance(noteMain)).commit();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_description, menu);
+        MenuItem item = menu.findItem(R.id.toolbar_about);
+        if (item != null) {
+            item.setVisible(false);
         }
     }
 
