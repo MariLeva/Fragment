1892b1a8724184caf2c80338cb9d425bd5752c71
mari_leva <savosh999@yandex.ru>
2022-02-18 22:23:52 +0700

    Урок 8. Добавила свое меню в активити.

--- app/src/main/java/ru/geekbrains/fragment/MainActivity.java
+++ app/src/main/java/ru/geekbrains/fragment/MainActivity.java
@@ -1,9 +1,13 @@
 package ru.geekbrains.fragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_notes, new Notes()).commit();
         }
+
+        setSupportActionBar(findViewById(R.id.toolBar));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.toolbar_about:
+                getSupportFragmentManager().beginTransaction().addToBackStack("").add(R.id.fragment_notes, new AboutFragment()).commit();
+                return true;
+            case R.id.toolbar_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
