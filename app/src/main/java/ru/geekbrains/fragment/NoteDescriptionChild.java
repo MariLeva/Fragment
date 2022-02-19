c9a4a5649b1a640cc777cfe22f6516b07e924af1
mari_leva <savosh999@yandex.ru>
2022-02-19 12:50:19 +0700

    Урок 8. Добавила popupMenu при долгом нажатие на описание заметки. Так же создала свой стиль для всплывающих меню

--- app/src/main/java/ru/geekbrains/fragment/NoteDescriptionChild.java
+++ app/src/main/java/ru/geekbrains/fragment/NoteDescriptionChild.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,8 +8,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 public class NoteDescriptionChild extends Fragment {
@@ -31,6 +34,7 @@ public class NoteDescriptionChild extends Fragment {
             String[] note_description = getResources().getStringArray(R.array.note_description);
             tvNote.setText(note_description[noteMain.getIndex()]);
         }
+        initPopupMenu(view);
     }
 
     public static NoteDescriptionChild newInstance(NoteMain noteMain) {
@@ -39,5 +43,28 @@ public class NoteDescriptionChild extends Fragment {
         args.putParcelable(ARG_INDEX_CHILD, noteMain);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    private void initPopupMenu(View view) {
+        TextView textView = view.findViewById(R.id.description);
+        textView.setOnClickListener(v -> {
+                    Activity activity = requireActivity();
+                    PopupMenu popupMenu = new PopupMenu(activity, v);
+                    activity.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            switch (menuItem.getItemId()) {
+                                case R.id.popup_clean:
+                                    return true;
+                                case R.id.popup_edit:
+                                    return true;
+                            }
+                            return true;
+                        }
+                    });
+                    popupMenu.show();
+                }
+        );
     }
 }
